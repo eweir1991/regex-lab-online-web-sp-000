ce0e8ba4d2abee402df52aad399bac20f2172a4f
Erik Weir <erik.weir@key.me>
2020-04-27 19:16:28 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,12 +15,11 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/^[A-Z].+[\.!?]$/).empty?
-    return true
+if text.scan(/^[A-Z][\s\S]+\.$/).empty?
+    false
   else
-    return false
+    true
   end
-
 end
 
 def valid_phone_number?(phone)
