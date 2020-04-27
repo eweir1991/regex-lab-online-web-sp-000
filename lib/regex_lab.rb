6be106a60d766c8d3a968e626d83df164d36a9cb
Erik Weir <erik.weir@key.me>
2020-04-27 19:07:17 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/^[A-Z]. +[\.!?]$/) then
+  if text.match(/^[A-Z].+[\.!?]$/).empty?
     return true
   else
     return false
@@ -24,5 +24,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-
+  phone.scan
 end
